9aeacd39b0516ed73195026975a7333b8a079846
renatodex <renatodex@gmail.com>
2013-12-29 20:42:55 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LuluRpg::Application.config.secret_key_base = '4d9a78e0b2e4a6c26e602f22a740ab9d55f57bba4dbb7818b07bfdbb1c8054b8662b9fe20fa410492838ef38adf8aaa9fec25f41a440655ced9611a42c168463'
